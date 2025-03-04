53bbd55e4220ed2f95e1e0c13afcb8fefb737a80
sfsdafd <88030392+sfsdafd@users.noreply.github.com>
2025-03-04 19:20:05 +0800

    Update Encrypt.java

--- src/main/java/com/summersec/attack/Encrypt/Encrypt.java
+++ src/main/java/com/summersec/attack/Encrypt/Encrypt.java
@@ -1,40 +1,40 @@
 package com.summersec.attack.Encrypt;
 
-import sun.misc.BASE64Encoder;
-
 import javax.crypto.Cipher;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 import java.security.SecureRandom;
+import java.util.Base64;
 
 /**
- * @ClassName: encrypy
- * @Description: TODO
+ * @ClassName: Encrypt
+ * @Description: AES加密工具类
  * @Author: Summer
  * @Date: 2021/1/19 20:25
  * @Version: v1.0.0
- * @Description:
  **/
 public class Encrypt {
 
     public static String encrypt(byte[] serialized, byte[] key) throws Exception {
-
-        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");//"算法/模式/补码方式"
+        Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5Padding");
 
         int sizeInBytes = 16;
         byte[] iv = new byte[sizeInBytes];
         SecureRandom random = new SecureRandom();
         random.nextBytes(iv);
+
         SecretKeySpec skeySpec = new SecretKeySpec(key, "AES");
-        //偏移量
-        IvParameterSpec ivSpec =  new IvParameterSpec(iv);
+        IvParameterSpec ivSpec = new IvParameterSpec(iv);
+
         cipher.init(Cipher.ENCRYPT_MODE, skeySpec, ivSpec);
         byte[] encrypted = cipher.doFinal(serialized);
-        byte[] output;
-        output = new byte[iv.length + encrypted.length];
+
+        // 合并 IV 和加密数据
+        byte[] output = new byte[iv.length + encrypted.length];
         System.arraycopy(iv, 0, output, 0, iv.length);
         System.arraycopy(encrypted, 0, output, iv.length, encrypted.length);
-        return (new BASE64Encoder().encode(output)).replaceAll("\r\n","");
-    }
 
+        // 使用 Java 8+ 的 Base64 进行编码
+        return Base64.getEncoder().encodeToString(output);
+    }
 }
